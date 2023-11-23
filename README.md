fbd2e9ef3b6fb54a8a195cd7694beec6dad403b3
RaphMt <rafa.mtorres117@outlook.com>
2023-11-23 20:17:09 -0300

    .

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# 💤 LazyVim
+# 💤 My LazyVim setup
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+Initial config for [LazyVim](https://github.com/LazyVim/LazyVim), still a work in progress.
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,53 +1,53 @@
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "astro",
-        "cmake",
-        "cpp",
-        "css",
-        "bash",
-        "gitignore",
-        "go",
-        "javascript",
-        "http",
-        "java",
-        "rust",
-        "sql",
-      },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "astro",
+                "cmake",
+                "cpp",
+                "css",
+                "bash",
+                "gitignore",
+                "go",
+                "javascript",
+                "http",
+                "java",
+                "rust",
+                "sql",
+            },
 
-      -- matchup = {
-      -- 	enable = true,
-      -- },
+            -- matchup = {
+            -- 	enable = true,
+            -- },
 
-      -- https://github.com/nvim-treesitter/playground#query-linter
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
+            -- https://github.com/nvim-treesitter/playground#query-linter
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
 
-      playground = {
-        enable = false,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
+            playground = {
+                enable = false,
+                disable = {},
+                updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                persist_queries = true, -- Whether the query persists across vim sessions
+                keybindings = {
+                    toggle_query_editor = "o",
+                    toggle_hl_groups = "i",
+                    toggle_injected_languages = "t",
+                    toggle_anonymous_nodes = "a",
+                    toggle_language_display = "I",
+                    focus_language = "f",
+                    unfocus_language = "F",
+                    update = "R",
+                    goto_node = "<cr>",
+                    show_help = "?",
+                },
+            },
         },
-      },
     },
-  },
 }
